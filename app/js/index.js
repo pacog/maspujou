ce4aa7eb1f507547d6be8277673033dccce64910
Paco Gomez Granados <pacogomezgranados@gmail.com>
2017-12-07 16:32:14 +0100

    Lazy loading of lightbox plugin, closing menu on click

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,21 @@
 (function() {
     'use strict';
+
     baguetteBox.run('.gallery');
+
+    closeMenuWhenLinkIsClicked();
+
+    function closeMenuWhenLinkIsClicked() {
+        var checkbox = document.querySelector('.landing-nav-mobile-button-checkbox');
+        var menu = document.querySelector('.landing-nav-content');
+        if(!checkbox || !menu) {
+            return;
+        }
+        menu.addEventListener('click', function() {
+            checkbox.checked = false;
+        });
+    }
+
+
+
 })();
